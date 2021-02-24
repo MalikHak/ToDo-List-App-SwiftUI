82c025e0bb55689d1c22356989ece140fb11f914
Malik2020 <malikfaisalhakim@gmail.com>
2021-02-24 11:09:50 +0430

    Add files via upload

---
+++ ToDo/ToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by Faisal Hakim on 2/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
